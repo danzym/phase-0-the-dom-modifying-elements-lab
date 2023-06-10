1b7180fce8925d62e50977e06cdac7abb0b0196c
Danylo Zymbalevskyi <zymbalevskyy@gmail.com>
2023-06-10 12:49:18 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.addEventListener("DOMContentLoaded", () => {
+  const mainElement = document.querySelector("main#main");
+  if (mainElement) {
+    mainElement.remove();
+  }
+
+  const newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  newHeader.innerHTML = "Dan is the champion";
+  document.body.appendChild(newHeader);
+});
